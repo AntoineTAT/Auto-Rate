625ba0ada40bdddfaaf26ce18d4442a4eafca5e0
antoine <antoine.tat@epitech.eu>
2021-08-11 02:45:42 +0200

    login form + couleur glob

--- front/components/molecules/RightHeader.jsx
+++ front/components/molecules/RightHeader.jsx
@@ -1,31 +1,31 @@
 import HeaderItem from "../atoms/HeaderItem"
 import Link from "next/link"
-import { HomeIcon , CollectionIcon, CurrencyEuroIcon } from "@heroicons/react/outline"
+import { LoginIcon, LogoutIcon, UserIcon } from "@heroicons/react/outline"
 import { useEffect, useState } from "react/cjs/react.development";
 import { useRouter } from "next/router"
 
-function MiddleHeader() {
+function RightHeader() {
     return (
         <div className="flex flex-grow justify-evenly max-w-2xl">
+            <Link href="/auth/login">
+                <a>
+                    <HeaderItem title="LOGIN" Icon={LoginIcon} />
+                </a>
+            </Link>
+
+            <Link href="/profile">
+                <a>
+                    <HeaderItem title="PROFILE" Icon={UserIcon} />
+                </a>
+            </Link>
+
             <Link href="/">
                 <a>
-                    <HeaderItem title="HOME" Icon={HomeIcon} />
-                </a>
-            </Link>
-
-            <Link href="/advertisement">
-                <a>
-                    <HeaderItem title="ADVERTISEMENT" Icon={CollectionIcon} />
-                </a>
-            </Link>
-
-            <Link href="/pricing">
-                <a>
-                    <HeaderItem title="PRICING" Icon={CurrencyEuroIcon} />
+                    <HeaderItem title="LOGOUT" Icon={LogoutIcon} />
                 </a>
             </Link>
         </div>
     )
 }
 
-export default MiddleHeader
+export default RightHeader
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/organisms/Header'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>AUTORATE</h1>
+      <Header/>
     </div>
   )
 }
